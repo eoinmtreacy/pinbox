d884c85cec58790eefb631e9033c788fe44c8a87
Daniel O'Flynn <danielpoflynn@gmail.com>
2024-06-02 17:33:41 +0100

    data quality report tool

--- ml_pipeline/data_evaluation/eval_utils/eval_utils.py
+++ ml_pipeline/data_evaluation/eval_utils/eval_utils.py
@@ -1,12 +1,20 @@
+"""Utilities used for Data Evaluation"""
+
 import pandas as pd
+import dataframe_image as dfi
 import matplotlib.pyplot as plt
 from docx import Document
+from docx.shared import Inches
 
 
 class DataAnalysis:
     def __init__(self, name: str, df: pd.DataFrame):
         self.name = name
         self.df = df
+        self.numeric_table_filename = f"{self.name}-DataQualityReport-NumericFeatures-Table.png"
+        self.category_table_filename = f"{self.name}-DataQualityReport-CategoricFeatures-Table.png"
+        self.histogram_filenames = []
+        self.boxplot_filenames = []
         self.initial_analysis()
 
     def get_numeric_columns(self):
@@ -70,6 +78,15 @@ class DataAnalysis:
         self.print_col_types()
         print("\n\n Convert column types to desired ones before continuing")
 
+    def save_df_to_png(self, df, name):
+        """Saves DataFrame to PNG image
+
+        Args:
+            df (pd.DataFrame): Dataframe to save
+            name (str): File name to save to
+        """
+        dfi.export(df, name)
+
     def numeric_desc_features(self):
         # First describe the numeric features
         self.df_table_numeric = self.df[self.numeric_columns].describe().T
@@ -87,6 +104,8 @@ class DataAnalysis:
         # Print data quality report table for numeric features to a file.
         df_numeric_columns_data_quality_report_table.to_csv(f"{self.name}-Amenities-DataQualityReport-NumericFeatures-Table.csv",
                                                             index_label='Feature')
+        self.save_df_to_png(df_numeric_columns_data_quality_report_table,
+                            self.numeric_table_filename)
         print(df_numeric_columns_data_quality_report_table)
 
     def categoric_desc_features(self):
@@ -126,8 +145,10 @@ class DataAnalysis:
         df_categoric_columns_data_quality_report_table = df_categoric_columns_data_quality_report_table[[
             'count', 'unique', 'top', 'freq', 'second', 'second_freq', '%missing', 'card']]
         # Print data quality report table for numeric features to a file.
-        df_categoric_columns_data_quality_report_table.to_csv(f"{self.name}-Amenities-DataQualityReport-CategoricFeatures-Table.csv",
+        df_categoric_columns_data_quality_report_table.to_csv(f"{self.name}-DataQualityReport-CategoricFeatures-Table.csv",
                                                               index_label='Feature')
+        self.save_df_to_png(df_categoric_columns_data_quality_report_table,
+                            self.category_table_filename)
 
     def print_value_proportions(self):
         print("\n\nValue proportions:")
@@ -141,12 +162,16 @@ class DataAnalysis:
         # Plot them all together instead
         plt.figure()
         self.df.hist(figsize=(30, 20))
-        plt.savefig(f"{self.name}_numeric_hist.png")
+        self.histogram_filenames = []
+        f = f"{self.name}_numeric_hist.png"
+        self.histogram_filenames.append(f)
+        plt.savefig(f)
 
     def plot_categoric(self):
         # Change code to produce new figure every 10 plots
         num_cols = 2
         plots_per_fig = 10
+        self.boxplot_filenames = []
         num_figures = (len(self.category_columns) +
                        plots_per_fig - 1)//plots_per_fig
 
@@ -173,10 +198,17 @@ class DataAnalysis:
             for j in range(len(current_cols), num_rows * 2):
                 fig.delaxes(axes[j])
 
-            plt.savefig(f"{self.name}_categoric_box_plot_{i}.png")
+            f = f"{self.name}_categoric_box_plot_{i}.png"
+            self.boxplot_filenames.append(f)
+            plt.savefig(f)
             plt.show()
 
     def analyse(self, plots=True):
+        """Performs analysis on the dataframe after initial cleaning and column type conversion is done.
+
+        Args:
+            plots (bool, optional): Whether or not to draw plots of the features. Defaults to True.
+        """
         self.get_numeric_columns()
 
         self.convert_object_cols()
@@ -196,6 +228,7 @@ class DataAnalysis:
         self.numeric_desc_features()
 
         self.categoric_desc_features()
+
         if plots:
             self.plot_numeric()
 
@@ -209,3 +242,189 @@ class DataQualityReport:
     def __init__(self, da: DataAnalysis, params):
         self.da = da
         self.params = params
+
+
+class DataQualityReport:
+    critical_params = ['title', 'description',
+                       'source', 'source_link', 'detailed_desc']
+    sig_digits = 5
+    drop_feature_cutoff = 50  # % of missing values allowed before feature is dropped
+
+    def __init__(self, da: DataAnalysis, params: dict):
+        self.da = da
+        self.params = params
+        # make sure all critical parameters are present
+        for param in self.critical_params:
+            if param not in params:
+                raise ValueError(
+                    f"The required value {param} is missing from parameters")
+
+        # Get the columns that contain null values
+        self.null_val_cols = []
+        for col in da.num_null_vals:
+            if da.num_null_vals[col] > 0:
+                self.null_val_cols.append(col)
+
+    def num_format(self, num):
+        return f'%.{self.sig_digits}g' % num
+
+    def write_missing_vals_overview(self):
+
+        if len(self.null_val_cols) == 0:
+            return "The dataset contains no missing values. "
+
+        if len(self.null_val_cols) == 1:
+            return f"The feature {self.null_val_cols[0]} contains missing values. "
+
+        if len(self.null_val_cols) > 5:
+            return f"{len(self.null_val_cols)} features contain missing values. "
+
+        output = "The features "
+        for col in self.null_val_cols:
+            output += f"{col}, "
+
+        output = output[:-2] + " contain missing values."
+
+    def write_df_shape(self):
+        return f"The dataset has {self.da.num_cols} features and {self.da.num_rows} rows. "
+
+    def write_missing_values_summary(self):
+        dropped_features = []
+        for col in self.null_val_cols:
+            if da.df_category_perc_missing.loc[col, '%missing'] > 50:
+                dropped_features.append(col)
+
+        if len(dropped_features) == 0:
+            return "No features will have to be dropped due to missing values. "
+
+        if len(dropped_features) < 5:
+            output = ""
+            for feature in dropped_features:
+                output += f"{feature}, "
+            output = output[:-2] + \
+                " will have to be dropped due to missing values. "
+            return output
+
+        return f"{len(dropped_features)} features will have to be dropped due to missing values. "
+
+    def write_duplicate_rows(self):
+        if (self.da.duplicate_count == 0):
+            return "There are no duplicate rows. "
+
+        return f"There are {self.da.duplicate_count} duplicate rows. "
+
+    def numeric_col_description(self, col):
+        output = f"This feature has a mean of {self.num_format(da.df_table_numeric.loc[col, 'mean'])}, a min value of {self.num_format(da.df_table_numeric.loc[col, 'min'])} and a max value of {self.num_format(da.df_table_numeric.loc[col, 'max'])}. "
+        if col in self.null_val_cols:
+            output += f"There are {da.num_null_vals[col]} missing values. "
+        else:
+            output += "There are no missing values. "
+
+        return output
+
+    def category_col_description(self, col):
+        output = f"This has {da.category_unique[col]} unique values. The most common is {da.df_table_categoric.loc[col,'top']}. "
+        if col in self.null_val_cols:
+            output += f"There are {da.num_null_vals[col]} missing values. "
+        else:
+            output += "There are no missing values. "
+
+        return output
+
+    def write_actions(self, document):
+
+        # Get rows to drop for missing vals
+        drop_row_cols = []
+        for col in self.critical_params:
+            if col in self.null_val_cols:
+                drop_row_cols.append(col)
+
+        cols_to_drop = da.df_category_perc_missing.loc[
+            da.df_category_perc_missing['%missing'] > 50].index.values
+
+        document.add_heading("Actions to Take", 2)
+        document.add_paragraph(
+            f"{len(cols_to_drop) + len(drop_row_cols)} actions will be taken:")
+        for col in drop_row_cols:
+            document.add_paragraph(col, style="List Bullet")
+            document.add_paragraph(
+                f"Drop rows with missing {col}. ", style="List Bullet 2")
+
+        for col in cols_to_drop:
+            document.add_paragraph(col, style="List Bullet")
+            document.add_paragraph(
+                f"Drop feature due to {self.num_format(da.df_category_perc_missing.loc[col, '%missing'])}% of values missing. ", style="List Bullet 2")
+
+        p = document.add_paragraph("")
+        p.add_run("***ADD ACTIONS TO TAKE***").bold = True
+
+    def add_appendix(self, document):
+        document.add_heading("Appendix", 1)
+        document.add_heading("Continuous Features", 2)
+        document.add_picture(da.numeric_table_filename, width=Inches(6.5))
+
+        document.add_heading("Descriptive Statistics", 3)
+        document.add_heading("Histograms", 3)
+        for f in da.histogram_filenames:
+            document.add_picture(f, width=Inches(6.5))
+
+        document.add_heading("Categorical Features", 2)
+        document.add_heading("Descriptive Statistics", 2)
+        document.add_picture(da.category_table_filename, width=Inches(6.5))
+        document.add_heading("Box Plots", 2)
+        for f in da.boxplot_filenames:
+            document.add_picture(f, width=Inches(5))
+
+    def write_document(self):
+        document = Document()
+
+        document.add_heading(f"{self.params['title']} Data Quality Report", 1)
+        document.add_heading('Overview', 2)
+
+        p = document.add_paragraph(
+            f"This report will outline the initial data quality findings on {self.params['description']} data obtained from {self.params['source']} which can be found at {self.params['source_link']}. ")
+        p.add_run(f'This report will include an overview of the dataset, and a review of the continuous and categorical features, including histograms and bar charts. On initial review, this dataset contains a lot of missing data for most features. The data that is present appears to be reasonable and logical, however a number of columns will need to be dropped. ')
+        mv = self.write_missing_vals_overview()
+        p.add_run(mv)
+
+        document.add_heading('Summary', 2)
+        p = document.add_paragraph(
+            f"This dataset consists of {self.params['detailed_desc']}. ")
+        p.add_run(self.write_df_shape())
+        p.add_run(self.write_missing_values_summary())
+        p.add_run(self.write_duplicate_rows())
+        p.add_run("Distribution of the data is consistent with expectations.")
+
+        document.add_heading('Review Logical Integrity', 2)
+        document.add_paragraph(
+            'Test 1: No date in x is before Feb 2022 or after the upload date (22nd May).')
+        document.add_paragraph('0 instances.', style='List Bullet')
+
+        if len(da.numeric_columns) > 0:
+            document.add_heading("Review Continuous Features", 2)
+            document.add_paragraph(
+                f"There are {len(da.numeric_columns)} continuous features in this dataset:")
+            for col in da.numeric_columns:
+                document.add_paragraph(col, style="List Bullet")
+                desc = self.numeric_col_description(col)
+                document.add_paragraph(desc, style="List Bullet 2")
+
+            document.add_heading("Histograms", 3)
+            p = document.add_paragraph(
+                "All Histograms can be found in the appendix as a summary sheet. All features show a plausible distribution.")
+            p.add_run("***REVIEW DISTRIBUTION***").bold = True
+
+        if len(da.category_columns) > 0:
+            document.add_heading("Review Continuous Features", 2)
+            document.add_paragraph(
+                f"There are {len(da.numeric_columns)} continuous features in this dataset:")
+            for col in da.category_columns:
+                document.add_paragraph(col, style="List Bullet")
+                desc = self.category_col_description(col)
+                document.add_paragraph(desc, style="List Bullet 2")
+
+        self.write_actions(document)
+
+        self.add_appendix(document)
+
+        document.save(f"{self.params['title']} Data Quality Report.docx")
