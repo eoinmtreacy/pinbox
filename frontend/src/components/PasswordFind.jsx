daf1f2d2337c12a2fdac137483985f6d7bc5c652
Philip Cyriac <philip.cyriac@ucdconnect.ie>
2024-06-11 21:53:36 +0100

    Revert "feat: Update Map component for enhanced responsiveness"
    This reverts commit 98165f9b2a5dfc8e70f3f9148feba4edf2d39621.

--- frontend/src/components/PasswordFind.jsx
+++ frontend/src/components/PasswordFind.jsx
@@ -1,91 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, useMap, GeoJSON } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import Header from './Header';
-import SearchBar from './SearchBar';
-import Sidebar from './Sidebar';
-import GetUserLocation from './GetUserLocation';
-import CookieModal from './CookieModal';
-import '../App.css';
-
-// Custom Zoom Control Component
-const CustomZoomControl = () => {
-    const map = useMap();
-
-    // Remove the default zoom control
-    useEffect(() => {
-        map.zoomControl.remove();
-    }, [map]);
-
-    return null;
-};
-
-// Update Leaflet icon paths to resolve missing icons
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
-const Map = () => {
-    const [geoJsonData, setGeoJsonData] = useState(null);
-
-    useEffect(() => {
-        // Fetch GeoJSON data
-        fetch('nightclub_amenities.geojson') // Ensure the file is in the public directory
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('GeoJSON data fetched:', data); // Debugging log
-                setGeoJsonData(data);
-            })
-            .catch((error) => {
-                console.error('Error fetching GeoJSON data:', error); // Debugging log
-            });
-    }, []);
-
+import * as React from 'react';
+import PasswordImg from '../Images/pw.png';
+function PasswordFind() {
     return (
-        <div className="relative flex flex-col md:flex-row h-screen w-screen">
-            {/* Fixed header at the top */}
-            <div className="absolute top-0 left-0 right-0 z-50 p-4">
-                <Header />
-            </div>
+        <div className="flex flex-col items-start py-20 pr-9 pl-20 mx-auto w-full text-4xl font-bold text-center bg-white max-w-[480px] rounded-[80px] text-stone-500">
+            <div className="self-center mt-12 tracking-tight text-black">Verify</div>
+            <img className="self-center mt-6 max-w-full aspect-[0.9] w-[289px]" src={PasswordImg} alt="passwordImg" />
+            <form>
+                <div className="mb-4">
+                    <div className="mt-3.5 text-xl">Enter your Name</div>
+                    <input
+                        type="name"
+                        id="name"
+                        className="border rounded w-full py-2 px-3 text-grey-darker"
+                        required
+                    />
+                </div>
 
-            {/* Fixed search bar below the header */}
-            <div className="absolute top-16 left-0 right-0 z-50 p-4">
-                <SearchBar />
-            </div>
+                <div className="mb-4">
+                    <div className="mt-3.5 text-xl">Enter your Email</div>
+                    <input
+                        type="name"
+                        id="name"
+                        className="border rounded w-full py-2 px-3 text-grey-darker"
+                        required
+                    />
+                </div>
+            </form>
 
-            {/* Sidebar, positioned responsively */}
-            <div className="md:relative md:w-[25%] bg-gray-200 p-4 z-40 h-full">
-                <Sidebar />
-            </div>
-
-            {/* Map container with responsive styling */}
-            <div className="flex-1">
-                <MapContainer center={[40.7478017, -73.9914126]} zoom={13} className="h-full w-full">
-                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                    {geoJsonData && (
-                        <GeoJSON
-                            data={geoJsonData}
-                            onEachFeature={(feature, layer) => {
-                                if (feature.properties && feature.properties.name) {
-                                    layer.bindPopup(`<b>${feature.properties.name}</b><br />${feature.properties.amenity}`);
-                                }
-                            }}
-                        />
-                    )}
-                    <GetUserLocation />
-                    <CustomZoomControl />
-                    {/* Fixed position for the cookie modal */}
-                    <div className="absolute bottom-4 left-0 right-0 z-50">
-                        <CookieModal />
-                    </div>
-                </MapContainer>
-            </div>
+            <button
+                className="justify-center self-center px-5 py-2.5 mt-14 text-white whitespace-nowrap border border-solid bg-indigo-500 border-slate-500 rounded-[30px]"
+                type="submit"
+            >
+                Submit
+            </button>
         </div>
     );
-};
-
-export default Map;
+}
+export default PasswordFind;
