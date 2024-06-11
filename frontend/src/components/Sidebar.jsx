4eb6e991a5dd48544896d9814c48bab5329b0d9c
Philip Cyriac <philip.cyriac@ucdconnect.ie>
2024-06-11 22:03:03 +0100

    feat: Improve SearchBar component for better responsiveness and maintainability
    - Updated the layout to use flexbox for vertical alignment of icons.
    - Added flex properties to center icons within their containers.
    - Removed absolute positioning to simplify layout and improve responsiveness.
    - Added consistent spacing between icons using margin-bottom.
    - Added detailed comments to explain the purpose of the container and each icon, enhancing maintainability.

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,28 +4,30 @@ import Cafe from '../Images/cafe.png';
 import Pub from '../Images/pub.png';
 import Park from '../Images/park.png';
 import Mall from '../Images/mall.png';
+
 const SearchBar = () => {
     return (
-        <div>
-            <div className="w-12 h-[237px] top-0 left-0 bg-[#d9d9d999] rounded-[20px]">
-                <div className="top-[191px] left-[9px] absolute w-[35px] h-[35px] bg-white rounded-[17.5px]">
-                    <img className="!absolute !w-[25px] !h-[26px] !top-[5px] !left-[5px]" src={Mall} alt="mall" />
+        <div className="relative">
+            <div className="w-12 h-[237px] bg-[#d9d9d999] rounded-[20px] flex flex-col items-center py-2">
+                {/* Restaurant Icon */}
+                <div className="mb-4 w-[35px] h-[35px] bg-white rounded-full flex items-center justify-center">
+                    <img className="w-[25px] h-[26px]" src={Restaurant} alt="restaurant" />
                 </div>
-                <div className="top-[59px] left-2 absolute w-[35px] h-[35px] bg-white rounded-[17.5px]">
-                    <img className="!absolute !w-[25px] !h-[26px] !top-[5px] !left-[5px]" src={Cafe} alt="cafe" />
+                {/* Cafe Icon */}
+                <div className="mb-4 w-[35px] h-[35px] bg-white rounded-full flex items-center justify-center">
+                    <img className="w-[25px] h-[26px]" src={Cafe} alt="cafe" />
                 </div>
-                <div className="top-[149px] left-2.5 absolute w-[35px] h-[35px] bg-white rounded-[17.5px]">
-                    <img className="!absolute !w-[25px] !h-[26px] !top-[5px] !left-[5px]" src={Park} alt="park" />
+                {/* Pub Icon */}
+                <div className="mb-4 w-[35px] h-[35px] bg-white rounded-full flex items-center justify-center">
+                    <img className="w-[25px] h-[26px]" src={Pub} alt="Pub" />
                 </div>
-                <div className="top-[107px] left-[9px] absolute w-[35px] h-[35px] bg-white rounded-[17.5px]">
-                    <img className="!absolute !w-[25px] !h-[26px] !top-[5px] !left-[5px]" src={Pub} alt="Pub" />
+                {/* Park Icon */}
+                <div className="mb-4 w-[35px] h-[35px] bg-white rounded-full flex items-center justify-center">
+                    <img className="w-[25px] h-[26px]" src={Park} alt="park" />
                 </div>
-                <div className="top-[11px] left-2 absolute w-[35px] h-[35px] bg-white rounded-[17.5px]">
-                    <img
-                        className="!absolute !w-[25px] !h-[26px] !top-[5px] !left-[5px]"
-                        src={Restaurant}
-                        alt="restaurant"
-                    />
+                {/* Mall Icon */}
+                <div className="w-[35px] h-[35px] bg-white rounded-full flex items-center justify-center">
+                    <img className="w-[25px] h-[26px]" src={Mall} alt="mall" />
                 </div>
             </div>
         </div>
