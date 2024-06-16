4ed691015ac749b9c0ae6177f1c0adbcb2048626
Philip Cyriac <philip.cyriac@ucdconnect.ie>
2024-06-17 00:19:23 +0100

    chore - update to sidewrapper

---
+++ frontend/src/components/SideBarWrapper.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import SideBar from './Sidebar';
+
+const SideBarWrapper = () => {
+    const location = useLocation();
+    console.log("Current Path:", location.pathname); // Debugging log
+
+    const noSidebarPaths = ['/login', '/signup', '/passwordfind', '/map'];
+    
+    if (noSidebarPaths.includes(location.pathname)) {
+        return null;
+    }
+
+    return <SideBar />;
+};
+
+export default SideBarWrapper;
