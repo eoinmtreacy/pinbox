dae630febe0b09ba1b1c14d3db1aa1accffda856
aida-coughlan <aidamariacoughlan@gmail.com>
2024-06-17 12:27:36 +0100

    Feat: YourModel class with first four column names of database table

--- backend/data/ApplicationDBContext.cs
+++ backend/data/ApplicationDBContext.cs
@@ -11,15 +11,11 @@ namespace backend.data
 
         public DbSet<YourModel> YourModels { get; set; }
     }
-
     public class YourModel
     {
         public int Id { get; set; }
-        
-        // Declare the Name property as nullable
-        public string? Name { get; set; }
-
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public string? Name { get; set; }
     }
 }
