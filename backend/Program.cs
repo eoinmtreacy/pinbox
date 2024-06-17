0c21af99c9b8045ff092ded9da4837938640c2d3
aida-coughlan <aidamariacoughlan@gmail.com>
2024-06-17 15:13:44 +0100

    feat: config.cs file added to read in variables for .env file

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,28 @@
+using DotNetEnv;
+using Microsoft.Extensions.Configuration;
+using backend;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+
+string dbPassword = Config.DbPassword;
+string dbHost = Config.DbHost;
+string dbPort = Config.DbPort;
+string dbName = Config.DbName;
+string dbUser = Config.DbUser;
+
+var connectionStringTemplate = builder.Configuration.GetConnectionString("DefaultConnection");
+
+string connectionString = connectionStringTemplate
+    .Replace("{DB_HOST}", dbHost)
+    .Replace("{DB_PORT}", dbPort)
+    .Replace("{DB_NAME}", dbName)
+    .Replace("{DB_USER}", dbUser)
+    .Replace("{DB_PASSWORD}", dbPassword);
+
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
