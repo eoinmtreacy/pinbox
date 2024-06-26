81a9f40786d7e0bf90f5053ea23835ac8de7a9ed
Philip Cyriac <philip.cyriac@ucdconnect.ie>
2024-06-26 01:42:31 +0100

    feat: Integrate Settings component into App routing
    - Add route for Settings component in App.jsx
    - Ensure Settings component is included in the app's navigation
    - Update routing to account for SideNav width with proper left margin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,23 +9,25 @@ import Main from './components/Main';
 import PasswordFind from './components/PasswordFind';
 import Preference from './components/Preference';
 import Profile from './components/Profile';
-import SideNav from './components/SideNav'; // Ensure SideNav is imported
+import Settings from './components/Settings';
+import SideNav from './components/SideNav';
 
 function App() {
     return (
         <div className="App flex flex-col min-h-screen">
             <Router>
-                <SideNav /> {/* Ensure SideNav is always displayed */}
-                <div className="flex flex-1">
-                    <div className="flex-1 ml-[70px]"> {/* Add left margin to account for SideNav width */}
+                <SideNav />
+                <div className="flex flex-1 ml-[70px]"> {/* Add left margin to account for SideNav width */}
+                    <div className="flex-1">
                         <Routes>
-                            <Route path="/" element={<Main />} /> {/* Main page */}
-                            <Route path="/map" element={<Map />} /> {/* Map page with SideNav */}
-                            <Route path="/login" element={<Login />} /> {/* Login page */}
-                            <Route path="/signup" element={<Signup />} /> {/* Signup page */}
-                            <Route path="/passwordfind" element={<PasswordFind />} /> {/* Password find page */}
-                            <Route path="/preference" element={<Preference />} /> {/* Preference page */}
-                            <Route path="/profile" element={<Profile />} /> {/* Profile page */}
+                            <Route path="/" element={<Main />} />
+                            <Route path="/map" element={<Map />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/signup" element={<Signup />} />
+                            <Route path="/passwordfind" element={<PasswordFind />} />
+                            <Route path="/preference" element={<Preference />} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="/settings" element={<Settings />} />
                         </Routes>
                     </div>
                 </div>
