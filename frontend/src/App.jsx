22fd56ab0df8732d86a10664d564e37fc65c97fa
Philip Cyriac <philip.cyriac@ucdconnect.ie>
2024-06-26 01:49:29 +0100

    fix: Conditionally render SideNav based on current route
    - Add logic to hide SideNav on login and signup pages
    - Use useLocation hook to determine current route
    - Adjust layout to account for SideNav presence

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 import Map from './components/Map';
@@ -16,24 +16,33 @@ function App() {
     return (
         <div className="App flex flex-col min-h-screen">
             <Router>
-                <SideNav />
-                <div className="flex flex-1 ml-[70px]"> {/* Add left margin to account for SideNav width */}
-                    <div className="flex-1">
-                        <Routes>
-                            <Route path="/" element={<Main />} />
-                            <Route path="/map" element={<Map />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/signup" element={<Signup />} />
-                            <Route path="/passwordfind" element={<PasswordFind />} />
-                            <Route path="/preference" element={<Preference />} />
-                            <Route path="/profile" element={<Profile />} />
-                            <Route path="/settings" element={<Settings />} />
-                        </Routes>
-                    </div>
-                </div>
+                <AppContent />
             </Router>
         </div>
     );
 }
+
+const AppContent = () => {
+    const location = useLocation();
+    const hideSideNav = ['/Login', '/Signup'].includes(location.pathname);
+
+    return (
+        <div className="flex flex-1">
+            {hideSideNav && <SideNav />}
+            <div className={!hideSideNav ? "flex-1 ml-[70px]" : "flex-1"}>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/map" element={<Map />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/passwordfind" element={<PasswordFind />} />
+                    <Route path="/preference" element={<Preference />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/settings" element={<Settings />} />
+                </Routes>
+            </div>
+        </div>
+    );
+};
 
 export default App;
