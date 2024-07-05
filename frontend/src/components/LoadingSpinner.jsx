090fbc1df27f9c812889b39bc13d088c641f35a1
Philip Cyriac <philip.cyriac@ucdconnect.ie>
2024-07-05 17:24:21 +0100

    Update LoadingSpinner.jsx

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -33,8 +33,8 @@ const LoadingSpinner = () => {
                     className="animate-spin-horizontal"
                     alt="Pinbox"
                     style={{
-                        width: '100%',
-                        height: '100%',
+                        width: '75%',
+                        height: '75%',
                     }}
                 />
             </div>
