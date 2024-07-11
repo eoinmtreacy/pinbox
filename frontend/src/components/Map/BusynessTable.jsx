90839fdeaa6659e789efe3778347de92602cae2a
gyuwon <kyn00018@gmail.com>
2024-07-11 18:55:22 +0100

    colourTable

---
+++ frontend/src/components/Map/BusynessTable.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const getColorAndDescription = (busyness) => {
+    if (busyness <= 0.5) {
+        return { color: 'green', description: 'Good' };
+    } else if (busyness <= 1) {
+        return { color: 'yellow', description: 'Moderate' };
+    } else if (busyness <= 1.5) {
+        return { color: 'orange', description: 'A liitle busy' };
+    } else {
+        return { color: 'red', description: 'Hectic' };
+    }
+};
+
+const BusynessModal = ({ isOpen, onRequestClose, busyness }) => {
+    const { color, description } = getColorAndDescription(busyness);
+
+    return (
+        <div className="preference-container flex flex-col bg-white rounded-2xl border border-solid border-stone-400 max-w-xs p-5 bg-opacity-80">
+            <div className="text-3xl font-bold  text-center text-black mb-1">Busyness Level</div>
+            <table className="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th className="border px-4 py-2">Busyness Value</th>
+                        <th className="border px-4 py-2">Colour & Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style={{ backgroundColor: 'green' }}>
+                        <td className="border px-4 py-2">0.0 ~ 0.5</td>
+                        <td className="border px-4 py-2">Good</td>
+                    </tr>
+                    <tr style={{ backgroundColor: 'yellow' }}>
+                        <td className="border px-4 py-2">0.5 ~ 1.0</td>
+                        <td className="border px-4 py-2">Moderate</td>
+                    </tr>
+                    <tr style={{ backgroundColor: 'orange' }}>
+                        <td className="border px-4 py-2">1.0 to 1.5</td>
+                        <td className="border px-4 py-2">A liitle busy</td>
+                    </tr>
+                    <tr style={{ backgroundColor: 'red' }}>
+                        <td className="border px-4 py-2">1.5 to 2.0</td>
+                        <td className="border px-4 py-2">Hectic</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default BusynessModal;
