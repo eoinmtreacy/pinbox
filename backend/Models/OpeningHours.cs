b70105f3468434c4aafa9095a3dceba60f5ee18d
gyuwon <kyn00018@gmail.com>
2024-07-17 16:19:30 +0100

    openhour nullable edited

--- backend/Models/OpeningHours.cs
+++ backend/Models/OpeningHours.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace backend.Models
 {
-    public class OpeningHour
-    {
-        public long Id { get; set; }  // Primary Key
-        public long PlaceId { get; set; }
-        public string Day { get; set; }
-        public TimeSpan OpenTime { get; set; }
-        public TimeSpan CloseTime { get; set; }
-        public Place Place { get; set; }
-        public string Status { get; set; }
-    }
+   public class OpeningHour
+{
+    public long Id { get; set; }  // Primary Key
+    public long PlaceId { get; set; }
+    public string? Day { get; set; }  // nullable
+    public TimeSpan OpenTime { get; set; }
+    public TimeSpan CloseTime { get; set; }
+    public Place? Place { get; set; }  // nullable
+    public string? Status { get; set; }  // nullable
+}
 }
