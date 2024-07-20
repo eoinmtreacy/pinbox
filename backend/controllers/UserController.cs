7737a26310095136d7d9c0fd04e90a0c196879b2
aida-coughlan <aidamariacoughlan@gmail.com>
2024-07-20 13:35:21 +0100

    fix: namespace + import

--- backend/controllers/UserController.cs
+++ backend/controllers/UserController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
