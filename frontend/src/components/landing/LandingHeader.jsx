a442b176a57087b2fbe37c5a741c6357eaa5bf31
Eoin Treacy <eointreacy5@gmail.com>
2024-07-27 14:02:00 +0100

    fix: title "ALL PINS" bug

--- frontend/src/components/landing/LandingHeader.jsx
+++ frontend/src/components/landing/LandingHeader.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Logo from '../../Images/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const LandingHeader = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
     const handleScrollToSection = (event, sectionId) => {
         event.preventDefault();
@@ -82,7 +84,7 @@ const LandingHeader = () => {
                     </div>
 
                     <button
-                        onClick={() => window.location.href = '/join'}
+                        onClick={() => navigate('/welcome')}
                         title="Join Now"
                         className="hidden lg:inline-flex items-center justify-center px-5 py-2.5 text-base transition-all duration-200 hover:bg-yellow-300 hover:text-black focus:text-black focus:bg-yellow-300 font-semibold text-white bg-black rounded-full"
                     >
